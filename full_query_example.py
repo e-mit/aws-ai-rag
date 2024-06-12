dfdfc44026839d12f26aa4969794e9846dcf8b43
e <6410762+e-mit@users.noreply.github.com>
2024-06-12 16:07:00 +0100

    Add initial LLM query to see if the input query relates to the news

--- full_query_example.py
+++ full_query_example.py
@@ -3,6 +3,8 @@ import json
 from typing import Any
 import datetime
 from datetime import timedelta
+import time
+import sys
 
 import boto3
 from botocore.exceptions import ClientError
@@ -13,14 +15,25 @@ NODE_URL = 'https://search-osstest2-oss-domain-2fgz4fbh4p2z3ul3z7goiutaay.eu-wes
 INDEX_NAME = "news"  # must be lower case
 AWS_REGION = "eu-west-3"
 
-LLM_MODEL_ID = "anthropic.claude-3-sonnet-20240229-v1:0"
+query = 'what has happened in the UK today and yesterday?'
 
-query = 'What has happened in France since 7th June?'
-
+# Search result selection parameters:
 QUERY_SIZE = 3
 QUERY_K = 3
+SCORE_THRESHOLD = 0.1
+
+# LLM parameters:
+LLM_MODEL_ID = "anthropic.claude-3-sonnet-20240229-v1:0"
+TEMPERATURE = 0.5
+TOP_P = 0.999
+TOP_K = 250
+MAX_OUTPUT_TOKENS = 1000
+TEXT_INPUT_FIELD = 'chunk'  # e.g. 'chunk' or 'full_text'
 
 #################################
+
+print()
+print(f"QUESTION: {query}")
 
 EMBEDDING_MODEL = {'id': "amazon.titan-embed-image-v1", 'dimension': 1024}
 embedding_client = boto3.client("bedrock-runtime", region_name=AWS_REGION)
@@ -42,16 +55,15 @@ client = OpenSearch(
 
 llm_client = boto3.client("bedrock-runtime", region_name="eu-west-3")
 
-#################################
 
 def invoke(prompt: str):
     native_request: dict[str, Any] = {
         "anthropic_version": "bedrock-2023-05-31",
-        "max_tokens": 1000,
+        "max_tokens": MAX_OUTPUT_TOKENS,
         "messages": [{"role": "user", "content": prompt}],
-        "temperature": 1.0,
-        "top_p": 0.999,
-        "top_k": 250
+        "temperature": TEMPERATURE,
+        "top_p": TOP_P,
+        "top_k": TOP_K
     }
 
     try:
@@ -59,23 +71,8 @@ def invoke(prompt: str):
                                            body=json.dumps(native_request))
     except (ClientError, Exception) as e:
         print(f"ERROR: Can't invoke '{LLM_MODEL_ID}'. Reason: {e}")
-        exit(1)
+        sys.exit()
     return json.loads(response["body"].read())
-
-
-today = datetime.datetime.now().strftime('%d %B %Y')
-date_prompt = (f"Today is {today}. Given the following query, what"
-               " date or dates are relevant when searching a database"
-               " of news articles? Return the date(s), as a Python"
-               " list of strings, without any other text."
-               " Return 'null', not in a list, if no dates are relevant."
-               f" Query: '{query}'")
-
-model_response = invoke(date_prompt)
-response_text = model_response["content"][0]["text"]
-dates = json.loads(response_text.replace("'", '"'))
-
-###########################################
 
 
 def apply_embedding(input_text: str) -> list[float]:
@@ -85,24 +82,59 @@ def apply_embedding(input_text: str) -> list[float]:
     return json.loads(response["body"].read())["embedding"]
 
 
-# query with filtering by date
+#################################
+# Identify inappropriate questions
+ok_prompt = (f"Does the following query relate to the news? Answer yes or no."
+             f" Do not return any other text. Query: '{query}'")
+ok_response = invoke(ok_prompt)
+ok_text: str = ok_response["content"][0]["text"]
 
-query_embedding = apply_embedding(query)
+if ok_text.lower() != "yes":
+    print("ANSWER: Please ask a question which relates to the news.")
+    sys.exit()
+
+###########################################
+# Identify relevant dates
+
+today = datetime.datetime.now().strftime('%d %B %Y')
+date_prompt = (f"Today is {today}. Given the following query, what"
+               " date or dates are relevant when searching a database"
+               " of news articles? Return the date(s), as a Python"
+               " list of strings, or"
+               " return 'null' if no dates are relevant."
+               " Do not return any other text."
+               f" Query: '{query}'")
+
+date_response = invoke(date_prompt)
+date_text = date_response["content"][0]["text"]
+dates = json.loads(date_text.replace("'", '"'))
+
+if not dates or dates == ['null']:
+    dates = []
+
+print()
+print(f"Found {len(dates)} relevant dates.")
+
+###########################################
+# Query the database, while also filtering by date (if applicable)
+
 query_body = {
     "size": QUERY_SIZE,
-    "query": {"knn": {"embedding": {"vector": query_embedding, "k": QUERY_K}}}
+    "query": {"knn": {"embedding": {
+        "vector": apply_embedding(query), "k": QUERY_K}}}
 }
 
-if dates is None:
+if not dates:
     results = client.search(
         body=query_body,
         index=INDEX_NAME,
-        _source="title,url,time_read,full_text",
+        _source=f"title,url,time_read,{TEXT_INPUT_FIELD}",
     )
     hits = results['hits']['hits']
+    print(f"Found {len(hits)} hits with scores of:"
+          f" {', '.join(str(h['_score']) for h in hits)}")
 else:
     all_hits = []
-    all_scores = []
     for d in dates:
         date_query_body = query_body
         day_start = datetime.datetime.strptime(str(d), "%Y-%m-%d")
@@ -113,7 +145,7 @@ else:
                     "range": {
                         "time_read": {
                             "gte": day_start.timestamp(),
-                            "lte": day_end.timestamp()
+                            "lt": day_end.timestamp()
                         }
                     }
                 }]
@@ -123,18 +155,19 @@ else:
         results = client.search(
             body=date_query_body,
             index=INDEX_NAME,
-            _source="title,url,time_read,full_text",
+            _source=f"title,url,time_read,{TEXT_INPUT_FIELD}",
         )
 
-        print(f"Date: {d} had {len(results['hits']['hits'])} hits.")
+        print(f"Date: {d} had {len(results['hits']['hits'])} hits, with scores of:"
+              f" {', '.join(str(h['_score']) for h in results['hits']['hits'])}")
 
         # check times:
         for hit in results['hits']['hits']:
             time_read = hit['_source']['time_read']
             ok = ((time_read >= day_start.timestamp())
-                  and (time_read <= day_end.timestamp()))
+                  and (time_read < day_end.timestamp()))
             if not ok:
-                print(f"NOT in time range: {hit['_source']['title']}")
+                print(f"Error: not in time range: {hit['_source']['title']}")
 
         all_hits.extend(results['hits']['hits'])
 
@@ -144,17 +177,26 @@ else:
 
 ###################################################
 
+hits = [x for x in hits if x['_score'] >= SCORE_THRESHOLD]
+print(f"After thresholding, have {len(hits)} hits, with scores of:"
+      f" {', '.join(str(h['_score']) for h in hits)}")
+print()
+
 if not hits:
     print("ANSWER: Sorry, I couldn't find any relevant news articles.")
+    sys.exit()
 else:
     combined_prompt = f"Today is {today}.\n\n" + "\n\n".join(
-        "BBC news article from"
+        "Summarized BBC news article from"
         f" {datetime.datetime.fromtimestamp(x['_source']['time_read']).strftime('%d %B %Y')}."
-        f"\n{x['_source']['full_text']}" for x in hits) + f"\n\nToday is {today}. {query}"
+        f"\n{x['_source'][TEXT_INPUT_FIELD]}" for x in hits) + f"\n\nToday is {today}. {query}"
 
+    timer_start = time.perf_counter()
     model_response = invoke(combined_prompt)
     print(f"ANSWER: {model_response['content'][0]['text']}")
     print()
-    print("This answer accessed the following source news articles:")
+    print(f"Query time: {time.perf_counter() - timer_start:0.4f} seconds")
+    print()
+    print("The model could access the following source news articles for this answer:")
     for hit in hits:
         print(f"  '{hit['_source']['title']}' {hit['_source']['url']}")
