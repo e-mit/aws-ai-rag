bbf76356e0969c2e697f4602b8adcf43011153af
e <6410762+e-mit@users.noreply.github.com>
2024-06-17 18:35:18 +0100

    Add functions for querying the LLM

---
+++ query_lambda/query.py
@@ -0,0 +1,81 @@
+"""Functions for querying the LLM."""
+
+from typing import Any
+import json
+import logging
+import os
+from datetime import datetime, date, timedelta
+
+import boto3
+
+from search_models import SearchHit
+import params
+
+logger = logging.getLogger()
+AWS_REGION = os.environ['AWS_REGION']
+
+llm_client = boto3.client("bedrock-runtime", region_name=AWS_REGION)
+
+native_request: dict[str, Any] = {
+    "anthropic_version": "bedrock-2023-05-31",
+    "max_tokens": params.MAX_OUTPUT_TOKENS,
+    "messages": [{"role": "user", "content": ""}],
+    "temperature": params.TEMPERATURE,
+    "top_p": params.TOP_P,
+    "top_k": params.TOP_K
+}
+
+
+def invoke_llm(prompt: str) -> dict[str, Any]:
+    """Send a prompt to the LLM and return the response."""
+    native_request['messages'][0]['content'] = prompt
+    try:
+        response = llm_client.invoke_model(modelId=params.LLM_MODEL_ID,
+                                           body=json.dumps(native_request))
+    except Exception as e:
+        logger.error('Prompt: %s', prompt)
+        raise e
+    return json.loads(response["body"].read())
+
+
+def is_question_appropriate(query: str) -> bool:
+    """"Identify inappropriate questions (not news related)."""
+    ok_prompt = ("Does the following query relate to the news? Answer yes"
+                 f" or no. Do not return any other text. Query: '{query}'")
+    ok_response = invoke_llm(ok_prompt)
+    ok_text = ok_response["content"][0]["text"]
+    return ok_text.lower() == "yes"
+
+
+def get_relevant_dates(query: str) -> list[date] | None:
+    """Find which dates, if any, are relevant to the query."""
+    today = datetime.now().strftime('%d %B %Y')
+    date_prompt = (
+         f"Today is {today}. Given the following query, what"
+         " date or dates are relevant when searching a database"
+         " of news articles? Return the date(s), as a Python"
+         " list of strings, or return 'null' if no dates are"
+         " relevant. Do not return any other text."
+         f" Query: '{query}'")
+
+    date_response = invoke_llm(date_prompt)
+    date_strings = json.loads(
+        date_response["content"][0]["text"].replace("'", '"'))
+
+    if not date_strings or date_strings == ['null']:
+        return None
+
+    dates = [datetime.strptime(x, "%Y-%m-%d").date() for x in date_strings]
+    expiry_date = (datetime.now().date()
+                   - timedelta(days=params.EXPIRY_PERIOD_DAYS))
+    return [x for x in dates if x >= expiry_date]
+
+
+def create_combined_prompt(query: str, hits: list[SearchHit]) -> str:
+    """Combine date, user query, and searched documents into one prompt."""
+    today = datetime.now().strftime('%d %B %Y')
+    return (f"Today is {today}.\n\n" + "\n\n".join(
+        "Summarized BBC news article from"
+        f" {datetime.fromtimestamp(x._source.time_read).strftime('%d %B %Y')}."
+        f"\n{x._source.chunk}" for x in hits)
+        + f"\n\nToday is {today}. {query}")
