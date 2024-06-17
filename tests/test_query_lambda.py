0da3a27d0b0abdae8d9ad2134edfd995e3b2e0f1
e <6410762+e-mit@users.noreply.github.com>
2024-06-17 19:57:17 +0100

    Add query lambda tests

---
+++ tests/test_query_lambda.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+os.environ['AWS_REGION'] = "eu-west-3"
+os.environ['OPENSEARCH_URL'] = 'https://search-osstest2-oss-domain-2fgz4fbh4p2z3ul3z7goiutaay.eu-west-3.es.amazonaws.com'
+
+sys.path.append("query_lambda")
+
+from query_lambda import lambda_function, params  # noqa
+
+
+def test_success():
+    result = lambda_function.lambda_handler(
+        {'query': 'Summarize the UK news yesterday'}, None)
+    print(result)  # NB: enable using: pytest -v -s tests
+    assert len(result['response']) > 0
+    assert isinstance(result['response'], str)
+    assert len(result['article_refs']) >= 1
+
+
+def test_inappropriate():
+    result = lambda_function.lambda_handler(
+        {'query': 'where can i buy pizza?'}, None)
+    assert result['response'] == params.INAPPROPRIATE_REPLY
+    assert len(result['article_refs']) == 0
+
+
+def test_no_results():
+    result = lambda_function.lambda_handler(
+        {'query': 'What was the news in mongolia in 1975?'}, None)
+    assert result['response'] == params.NO_RESULTS_REPLY
+    assert len(result['article_refs']) == 0
