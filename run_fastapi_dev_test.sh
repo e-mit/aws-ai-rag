90fb14e4aba38ff378d4fce7b4dcb587de448d21
e <6410762+e-mit@users.noreply.github.com>
2024-06-20 13:34:06 +0100

    Add temporary/dev password generator

--- run_fastapi_dev_test.sh
+++ run_fastapi_dev_test.sh
@@ -7,16 +7,24 @@ export DB_TABLE_NAME=testTable
 export TEST=true
 export LLM_LAMBDA_ARN=test
 
-export AUTH_SECRET_KEY='define this'
+TEMP_PWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 7; echo)
+BCRYPT_HASH=$(echo $TEMP_PWORD | python3 -c \
+"import sys
+from passlib.context import CryptContext
+print(CryptContext(schemes=['bcrypt'], deprecated='auto').hash(sys.stdin.read().strip()))")
+export AUTH_SECRET_KEY=$(openssl rand -hex 32)
 export AUTH_TOKEN_EXPIRE_MINS=2
-export AUTH_USER_PASSWORD_HASH='define this'
-export AUTH_ADMIN_PASSWORD_HASH='define this'
+export AUTH_USER_PASSWORD_HASH=$BCRYPT_HASH
+export AUTH_ADMIN_PASSWORD_HASH=$BCRYPT_HASH
 
 docker run --rm --name dynamodb_test_local -d -p 8000:8000 amazon/dynamodb-local
 sleep 5
 ./create_test_db_table.sh
 
-python fastapi_dev_test.py
-
-echo "NOTE: Stop and delete the test database container with:"
+echo ""
+echo "Log in with temporary password: $TEMP_PWORD"
+echo "Stop and delete the test database container with:"
 echo "docker stop -t 0 dynamodb_test_local"
+echo ""
+
+python fastapi_dev_test.py
