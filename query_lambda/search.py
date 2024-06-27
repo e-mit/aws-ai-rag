e535fc3b7d9b49c2700f9587cb6fbba10d5ad33a
e <6410762+e-mit@users.noreply.github.com>
2024-06-27 16:29:23 +0100

    Extract ABC for searches and hits, and make new GetAllSummaries, TimeSearch classes

--- query_lambda/search.py
+++ query_lambda/search.py
@@ -5,13 +5,14 @@ import logging
 import os
 from typing import Any
 from datetime import datetime, date
+from abc import ABC, abstractmethod
 
 import boto3
 from opensearchpy import OpenSearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth  # type: ignore
 
 from . import params
-from .search_models import SearchHit, Source
+from .search_models import SearchHit, Source, SourceSummary, SummarySearchHit
 
 logger = logging.getLogger()
 AWS_REGION = os.environ['AWS_REGION']
@@ -46,7 +47,40 @@ def apply_embedding(input_text: str) -> list[float]:
     return json.loads(response["body"].read())["embedding"]
 
 
-class Search:
+class SearchBase(ABC):
+    """Do an OSS database search, without specifying type."""
+
+    @abstractmethod
+    def _get_query_body(self) -> dict[str, Any]:
+        """Create the search query."""
+
+    def _run(self, return_summary_only: bool = False) -> list[dict[str, Any]]:
+        """Execute the search for full records or only summary part."""
+        if return_summary_only:
+            source = SourceSummary.get_attribute_string()
+        else:
+            source = Source.get_attribute_string()
+        results = os_client.search(
+            body=self._get_query_body(),
+            index=OSS_INDEX_NAME,
+            _source=source  # type: ignore
+        )
+        if results['timed_out']:
+            raise TimeoutError("Search timed out")
+        return results['hits']['hits']
+
+    def run(self) -> list[SearchHit]:
+        """Execute the search, returning full records."""
+        hit_results = self._run(return_summary_only=False)
+        return [SearchHit(**x) for x in hit_results]
+
+    def run_summary(self) -> list[SummarySearchHit]:
+        """Execute the search, returning only summary parts."""
+        hit_results = self._run(return_summary_only=True)
+        return [SummarySearchHit(**x) for x in hit_results]
+
+
+class Search(SearchBase):
     """Do a simple vector similarity search."""
 
     def __init__(self, query: str):
@@ -61,22 +95,12 @@ class Search:
                 "k": params.QUERY_K}}}
         }
 
-    def run(self) -> list[SearchHit]:
-        """Execute the search."""
-        results = os_client.search(
-            body=self._get_query_body(),
-            index=OSS_INDEX_NAME,
-            _source=Source.get_attribute_string()  # type: ignore
-        )
-        if results['timed_out']:
-            raise TimeoutError("Search timed out")
-        return [SearchHit(**x) for x in results['hits']['hits']]
-
 
 class DateFilteredSearch(Search):
     """Do a vector similarity search, also filtering by date range."""
 
     def __init__(self, query: str, start_day: date, end_day: date):
+        """Include both start and end days by using min/max times."""
         super().__init__(query)
         self.t1 = datetime.combine(start_day, datetime.min.time())
         self.t2 = datetime.combine(end_day, datetime.max.time())
@@ -97,3 +121,37 @@ class DateFilteredSearch(Search):
             }
         }
         return date_query_body
+
+
+class TimeSearch(SearchBase):
+    """Do a simple search by datetime range; not a similarity search."""
+
+    def __init__(self, start_time: datetime, end_time: datetime,
+                 size_per_shard: int = 1000):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.size_per_shard = size_per_shard
+
+    def _get_query_body(self) -> dict[str, Any]:
+        """Create the search query."""
+        return {"size": self.size_per_shard, "query": {
+                    "range": {
+                        "time_read": {"gte": self.start_time.timestamp(),
+                                      "lte": self.end_time.timestamp()}
+                    }
+                }}
+
+
+class GetAllSummaries(SearchBase):
+    """Get summaries for the whole database."""
+
+    def __init__(self, size_limit: int):
+        self.size_limit = size_limit
+
+    def _get_query_body(self) -> dict[str, Any]:
+        """Create the search query."""
+        return {"query": {"match_all": {}}, "size": self.size_limit}
+
+    def run(self) -> list[SearchHit]:
+        """Not appropriate."""
+        raise ValueError("Should only get summaries for all records.")
--- query_lambda/search_models.py
+++ query_lambda/search_models.py
@@ -1,6 +1,7 @@
 """Pydantic models for validating search results."""
 
-from typing import Any
+from typing import Any, Annotated
+from abc import ABC, abstractmethod
 
 from pydantic import BaseModel, field_validator, HttpUrl, Field
 
@@ -14,25 +15,24 @@ class SourceSummary(BaseModel):
     title: str
     url: HttpUrl
 
-
-class Source(SourceSummary):
-    """Represents the selected source data in the search result."""
-
-    chunk: str
-
     @classmethod
     def get_attribute_string(cls):
         """List what data are required in the search result."""
         return ",".join(cls.model_fields.keys())
 
 
-class SearchHit(BaseModel):
+class Source(SourceSummary):
+    """Represents the selected source data in the search result."""
+
+    chunk: str
+
+
+class Hit(ABC, BaseModel):
     """Represents an OpenSearch search hit."""
 
     index: str = Field(alias="_index")
     id: str = Field(alias="_id")
     score: float = Field(alias="_score")
-    source: Source = Field(alias="_source")
 
     @field_validator('index')
     @classmethod
@@ -42,7 +42,27 @@ class SearchHit(BaseModel):
             raise ValueError(f'Unexpected search index: {v}')
         return v
 
+    @abstractmethod
     def get_article_summary(self) -> dict[str, Any]:
-        """Get just the news-article-related information."""
+        """Get just the brief news-article-related information."""
+
+
+class SearchHit(Hit):
+    """Represents an OpenSearch search hit, with a full record."""
+
+    source: Source = Field(alias="_source")
+
+    def get_article_summary(self) -> dict[str, Any]:
+        """Get just the brief news-article-related information."""
         return {k: getattr(self.source, k) for k in
                 SourceSummary.model_fields}
+
+
+class SummarySearchHit(Hit):
+    """Represents an OpenSearch search hit, with a summary record."""
+
+    source: Annotated[SourceSummary, Field(alias="_source")]
+
+    def get_article_summary(self) -> dict[str, Any]:
+        """Get just the brief news-article-related information."""
+        return self.source.model_dump()
